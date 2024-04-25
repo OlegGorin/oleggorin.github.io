48514e58cd442ebc414b69eb505f5ee925463b08
OlegGorin <155927000+OlegGorin@users.noreply.github.com>
2024-04-25 17:32:10 +0200

    Add files via upload

---
+++ api.js
@@ -0,0 +1,144 @@
+// Замени на свой, чтобы получить независимый от других набор данных.
+// "боевая" версия инстапро лежит в ключе prod
+// const personalKey = "prod";
+const personalKey = "OlegGorin";
+const baseHost = "https://webdev-hw-api.vercel.app";
+const postsHost = `${baseHost}/api/v1/${personalKey}/instapro`;
+
+export function getPosts({ token }) {
+  return fetch(postsHost, {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+  })
+    .then((response) => {
+      if (response.status === 401) {
+        throw new Error("Нет авторизации");
+      }
+
+      return response.json();
+    })
+    .then((data) => {
+      return data.posts;
+    });
+}
+
+export function getUserPosts(id, { token }) {
+  return fetch(`${postsHost}/user-posts/${id}`, {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+  })
+    .then((response) => {
+      if (response.status === 401) {
+        throw new Error("Нет авторизации");
+      }
+
+      return response.json();
+    })
+    .then((data) => {
+      return data.posts;
+    });
+}
+
+// https://github.com/GlebkaF/webdev-hw-api/blob/main/pages/api/user/README.md#%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D1%8C%D1%81%D1%8F
+export function registerUser({ login, password, name, imageUrl }) {
+  return fetch(baseHost + "/api/user", {
+    method: "POST",
+    body: JSON.stringify({
+      login,
+      password,
+      name,
+      imageUrl,
+    }),
+  }).then((response) => {
+    if (response.status === 400) {
+      throw new Error("Такой пользователь уже существует");
+    }
+    return response.json();
+  });
+}
+
+export function loginUser({ login, password }) {
+  return fetch(baseHost + "/api/user/login", {
+    method: "POST",
+    body: JSON.stringify({
+      login,
+      password,
+    }),
+  }).then((response) => {
+    if (response.status === 400) {
+      throw new Error("Неверный логин или пароль");
+    }
+    return response.json();
+  });
+}
+
+// Загружает картинку в облако, возвращает url загруженной картинки
+export function uploadImage({ file }) {
+  const data = new FormData();
+  data.append("file", file);
+
+  return fetch(baseHost + "/api/upload/image", {
+    method: "POST",
+    body: data,
+  }).then((response) => {
+    return response.json();
+  });
+}
+
+export const postPosts = ({ token, description, imageUrl }) => {
+  return fetch(postsHost, {
+    method: "POST",
+    body: JSON.stringify({
+      description,
+      imageUrl,
+    }),
+    headers: {
+      Authorization: token,
+    },
+  }).then((response) => {
+    if (response.status === 500) {
+      throw new Error("Сервер недоступен");
+    } else if (response.status === 400) {
+      throw new Error("Плохой запрос");
+    } else {
+      return response.json();
+    }
+  });
+};
+
+export const toggleLike = ({ token }, likeId, doLike) => {
+  console.log(likeId, doLike);
+  let path = `${postsHost}/${likeId}/${doLike}`;
+  console.log(path);
+  return fetch(`${postsHost}/${likeId}/${doLike}`, {
+    method: "POST",
+    headers: {
+      Authorization: token,
+    },
+  }).then((response) => {
+    if (response.status === 200) {
+      return response.json();
+    } else {
+      throw new Error("Ставить лайки могут только авторизованные пользователи");
+    }
+  });
+};
+
+export function deletePost({ token }, id) {
+  return fetch(`${postsHost}/${id}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: token,
+    },
+  }).then((response) => {
+    if (response.status === 200) {
+      return response.json();
+    } else if (response.status === 500) {
+      throw new Error("Сервер недоступен");
+    }
+  });
+};
---
+++ components/posts-page-component.js
@@ -0,0 +1,159 @@
+import { USER_POSTS_PAGE } from "../routes.js";
+import { renderHeaderComponent } from "./header-component.js";
+import { posts, goToPage, page, likePost, id, delPost } from "../index.js";
+import { user } from "../index.js";
+import { formatDistance } from "date-fns";
+// Require Russian locale
+import { ru } from "date-fns/locale";
+
+export function renderPostsPageComponent({ appEl }) {
+  // TODO: реализовать рендер постов из api
+  console.log("Актуальный список постов:", posts);
+  
+  let postsHTML = posts
+    .map((post) => {
+      return `
+        <li class="post">
+        ${
+          page != USER_POSTS_PAGE
+            ? `<div class="post-header" data-user-id=${post.user.id}>
+            <img src=${post.user.imageUrl} class="post-header__user-image">
+            <p class="post-header__user-name">${post.user.name}</p>
+          </div>`
+            : ""
+        }
+          <div class="post-image-container">
+            <img class="post-image" src=${post.imageUrl}>
+          </div>
+          <div class="post-likes">
+            <button data-post-id=${post.id} data-post-liked="${
+        post.isLiked
+      }" class="like-button">
+
+            ${
+              post.isLiked
+                ? `<img src="./assets/images/like-active.svg"></img>`
+                : `<img src="./assets/images/like-not-active.svg"></img>`
+            }
+
+            </button>
+            <p class="post-likes-text">
+              Нравится: <strong>
+
+              ${
+                post.likes.length === 0
+                  ? 0
+                  : post.likes.length === 1
+                  ? post.likes[0].name
+                  : post.likes[post.likes.length - 1].name +
+                    " и ещё " +
+                    (post.likes.length - 1)
+              }
+
+              </strong>
+            </p>
+           </div>
+           <div class="footer">
+            <div>
+              <p class="post-text">
+                <span class="user-name">            
+                ${post.user.name}
+                </span>
+                ${post.description}
+              </p>
+              <p class="post-date">
+                ${formatDistance(new Date(), new Date(post.createdAt), {
+                  locale: ru,
+                })} назад
+              </p>
+            </div>  
+            <div>
+            ${
+              user
+                ? user._id === post.user.id
+                  ? `<button data-id=${post.id} class="button">
+                <div title="Удалить пост"></div>
+                Удалить</button>`
+                  : `<button data-id=${post.id} class="button-nonactive">
+                <div title="Вы можете удалить пост, созданный Вами"</div>
+                Удалить</button>`
+                : ""
+            }
+            </div>
+          </div>
+        </li>`;
+    })
+    .join("");
+
+  console.log(posts);
+
+  let userPosts = posts.map((post) => post);
+
+  let idUser = userPosts[0].user.id;
+  let nameUser = userPosts[0].user.name;
+  let imageUser = userPosts[0].user.imageUrl;
+
+  const appHtml = `
+    <div class="page-container">
+      <div class="header-container"></div>
+      <ul class="posts">
+      <li class="post">
+      ${
+        page === USER_POSTS_PAGE
+          ? `<div class="posts-user-header" data-user-id=${idUser}>
+          <img src=${imageUser} class="posts-user-header__user-image">
+          <p class="posts-user-header__user-name">${nameUser}</p>
+        </div>`
+          : ""
+      }
+
+        ${postsHTML}
+        </li>
+      </ul>
+    </div>`;
+
+  appEl.innerHTML = appHtml;
+
+  /**
+   * TODO: чтобы отформатировать дату создания поста в виде "19 минут назад"
+   * можно использовать https://date-fns.org/v2.29.3/docs/formatDistanceToNow
+   */
+
+  renderHeaderComponent({
+    element: document.querySelector(".header-container"),
+  });
+
+  for (let userEl of document.querySelectorAll(".post-header")) {
+    userEl.addEventListener("click", () => {
+      goToPage(USER_POSTS_PAGE, {
+        userId: userEl.dataset.userId,
+      });
+    });
+  }
+
+  for (let like of document.querySelectorAll(".like-button")) {
+    like.addEventListener("click", (event) => {
+      event.stopPropagation();
+      console.log(like.dataset);
+      let likeId = like.dataset.postId;
+      let likeIsLiked = like.dataset.postLiked;
+      let doLike = "";
+      if (likeIsLiked === "false") {
+        doLike = "like";
+      } else {
+        doLike = "dislike";
+      }
+      console.log(likeId, doLike);
+      likePost(likeId, doLike, page);
+    });
+  }
+
+  for (let del of document.querySelectorAll(".button")) {
+    del.addEventListener("click", (event) => {
+      event.stopPropagation();
+      console.log(del.dataset);
+      let delId = del.dataset.id;
+      delPost(delId, page);
+    });
+  }
+}
